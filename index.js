05b5108ba5ef3ab609b47c59ebcf8525ff94493e
zohaib ashraf <zohaib@nclouds.com>
2022-02-08 00:42:01 +0500

    new changing

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const appVersion = "4.0";
 
 app.get('/', (req, res) => {
 
-  var msg = `<h1>Hello zohaib this is node application version v3</h1>
+  var msg = `<h1>Hello zohaib this is node application version v3new changing</h1>
    <h2>
     Process ID: ${pid} <br> 
     Running on: ${hostname} <br>
