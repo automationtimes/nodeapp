07d98fe3d0b3114e653fab740bf49ce01f74ab40
zohaib ashraf <zohaib@nclouds.com>
2022-02-11 04:37:18 +0500

    slack set

---
+++ vars/slack.groovy
@@ -0,0 +1,10 @@
+def slack_code(
+       slackSend(
+                 color: color_slack_msg(),
+                 message: """
+                     *${currentBuild.currentResult}:* Job `${env.JOB_NAME}` build `${env.BUILD_DISPLAY_NAME}`>
+                      More info at: ${env.BUILD_URL}
+                      Time: ${currentBuild.durationString.minus(' and counting')}
+                      """
+              )
+)
