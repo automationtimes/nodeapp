66ef1012670d7f447dbe3410ec880cfa251b77db
zohaib ashraf <zohaib@nclouds.com>
2022-02-11 06:00:51 +0500

    update groovy script

---
+++ vars/color_slack_msg.groovy
@@ -0,0 +1,14 @@
+def color_slack_msg() {
+    switch(currentBuild.currentResult) {
+    case "SUCCESS":
+        return "good"
+        break
+    case "FAILURE":
+    case "UNSTABLE":
+        return "danger"
+        break
+    default:
+        return "warning"
+        break
+    }
+}
