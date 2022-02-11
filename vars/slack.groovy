685afe650b4676e282c95446fc50188eb52db026
zohaib ashraf <zohaib@nclouds.com>
2022-02-11 05:07:42 +0500

    update

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -8,3 +8,17 @@ def slack_code(){
                       """
               )
 }
+def color_slack_msg() {
+    switch(currentBuild.currentResult) {
+    case "SUCCESS":
+        return "good"
+        break
+    case "FAILURE":
+    case "UNSTABLE":
+        return "danger"
+        break
+    default:
+        return "warning"
+        break
+    }
+}
