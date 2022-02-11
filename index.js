45f4a23a2d4f6c331c1726e7a0dcc425188f3d9a
zohaib ashraf <zohaib@nclouds.com>
2022-02-12 00:29:46 +0500

    update file

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const appVersion = "4.0";
 
 app.get('/', (req, res) => {
 
-  var msg = `<h1>Hello zohaib this is node application version v15</h1>
+  var msg = `<h1>Hello zohaib this is node application version v15 trigger with ec2 plugin</h1>
    <h2>
     Process ID: ${pid} <br> 
     Running on: ${hostname} <br>
