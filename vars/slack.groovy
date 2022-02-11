90a58ee888d7570e60c7a69f0938f0e4fd1c0f77
zohaib ashraf <zohaib@nclouds.com>
2022-02-11 06:02:40 +0500

    update in groovy file

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -1,6 +1,6 @@
 def call(){
        slackSend(
-                 color: color_slack_msg(),
+                 color: color_slack_msg.call(),
                  message: """
                      *${currentBuild.currentResult}:* Job `${env.JOB_NAME}` build `${env.BUILD_DISPLAY_NAME}`>
                       More info at: ${env.BUILD_URL}
