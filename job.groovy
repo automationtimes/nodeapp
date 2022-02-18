efe753f474dd50eb6d727dbf5ee46958d97f59e0
zohaib ashraf <zohaib@nclouds.com>
2022-02-19 04:45:28 +0500

    update file

--- job.groovy
+++ job.groovy
@@ -1,17 +1,20 @@
-job('testJob1') {
-    scm {
-        git('https://github.com/automationtimes/nodeapp'){  node -> 
+job('DSL example') {    
+scm {        
+git('https://github.com/automationtimes/nodeapp') {  node -> 
       // is hudson.plugins.git.GitSCM            
   node / gitConfigName('zohaib')            
   node / gitConfigEmail('zohaib@gmail.com')        
     }    
 }
 triggers {          
-  scm('* * * * *')    
+  scm('H/5 * * * *')    
 }    
 wrappers {        
 nodejs('nodejs') 
+// this is the name of the NodeJS installation in                                                // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name    }    
 steps {        
 shell("npm install")    
   }
 }
+
+
