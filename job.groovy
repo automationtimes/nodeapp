a6b3f806ea69c4f6e38b7436d77221a9e8a4082e
zohaib ashraf <zohaib@nclouds.com>
2022-02-19 04:40:26 +0500

    update file

--- job.groovy
+++ job.groovy
@@ -1,15 +1,17 @@
-jobs:
-    job('testJob1') {
-        scm {
-            git('https://github.com/automationtimes/nodeapp')
-        }
-        branch('*/master')
-        triggers {
-            scm('* * * * *')
-        }
-    }
-    job('build') {
-        steps {
-            sh 'npm install'
-        }
-    }
+job('testJob1') {
+    scm {
+        git('https://github.com/automationtimes/nodeapp'){  node -> 
+      // is hudson.plugins.git.GitSCM            
+  node / gitConfigName('zohaib')            
+  node / gitConfigEmail('zohaib@gmail.com')        
+    }    
+}
+triggers {          
+  scm('* * * * *')    
+}    
+wrappers {        
+nodejs('nodejs') 
+steps {        
+shell("npm install")    
+  }
+}
