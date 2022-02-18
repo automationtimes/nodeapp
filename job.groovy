78b95166770b83b7ab320c0a46654ab02eeeccd3
zohaib ashraf <zohaib@nclouds.com>
2022-02-19 04:52:43 +0500

    update file

--- job.groovy
+++ job.groovy
@@ -1,20 +1,17 @@
-job('DSL example') {    
-scm {        
-git('https://github.com/automationtimes/nodeapp') {  node -> 
-      // is hudson.plugins.git.GitSCM            
-  node / gitConfigName('zohaib')            
-  node / gitConfigEmail('zohaib@gmail.com')        
-    }    
-}
-triggers {          
-  scm('H/5 * * * *')    
-}    
-wrappers {        
-nodejs('nodejs') 
-// this is the name of the NodeJS installation in                                                // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name    }    
-steps {        
-shell("npm install")    
-  }
-}
+pipelineJob('DSL_Demo') {
+
+    def repo = ' https://github.com/automationtimes/nodeapp'
+   
+    triggers {
+        scm('* * * * *')
+    }
+    
+    definition {
+        cpsScm {
+          scm {
+            git(repo, 'master', { node -> node / 'extensions' << '' } )
+            }
+        }
+    }
 
 
